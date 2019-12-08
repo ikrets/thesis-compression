6b5e081115c3bfc462ee5b4bae80f515457f8ea7
Ilia Krets <mrchair2170@gmail.com>
2019-12-08 21:01:48 +0100

    Fix saving experiment

--- experiment.py
+++ experiment.py
@@ -2,12 +2,14 @@ import subprocess
 import json
 from pathlib import Path
 
+
 def save_experiment_params(log_dir, args):
     log_dir = Path(log_dir)
     with (log_dir / 'parameters.json').open('w') as fp:
         params = vars(args)
-        params['commit'] = subprocess.run('git rev-parse HEAD', shell=True, stdout=subprocess.PIPE).stdout
-        json.dump(vars(args), fp, indent=4)
+        params['commit'] = subprocess.run('git rev-parse HEAD', shell=True, stdout=subprocess.PIPE).stdout.decode(
+            'ascii')
+        json.dump(params, fp, indent=4)
 
     with (log_dir / 'patch').open('w') as fp:
-        fp.write(subprocess.run('git diff', shell=True, stdout=subprocess.PIPE).stdout)
+        fp.write(subprocess.run('git diff', shell=True, stdout=subprocess.PIPE).stdout.decode('ascii'))
