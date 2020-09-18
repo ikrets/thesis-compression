9dec8bdc326bcf42e503b8e6209cf06666fe49c7
Ilia Krets <mrchair2170@gmail.com>
2020-09-19 01:34:42 +0200

    Implement cross-architecture evaluations

--- evaluator.py
+++ evaluator.py
@@ -1,10 +1,8 @@
 import argparse
 import tensorflow.compat.v1 as tf
 import numpy as np
-import json
 from pathlib import Path
 import pandas as pd
-from tqdm import tqdm
 
 import datasets.cifar10
 from experiment import save_experiment_params
@@ -15,12 +13,17 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--uncompressed_dataset', type=str, required=True)
-parser.add_argument('--downstream_O_model', type=str, required=True)
+parser.add_argument('--downstream_O_model')
 parser.add_argument('--downstream_O_model_weights', type=str)
 parser.add_argument('--downstream_O_model_correct_bgr', action='store_true')
-parser.add_argument('--compressed_dataset', type=str, required=True)
-parser.add_argument('--compressed_dataset_type', choices=['files', 'tfrecords'], default='tfrecords')
+parser.add_argument('--compressed_dataset_1', type=str, required=True,
+                    help='the compressed dataset used to train the downstream_C_model')
+parser.add_argument('--compressed_dataset_1_type', choices=['files', 'tfrecords'], default='tfrecords')
+parser.add_argument('--compressed_dataset_2', type=str, required=True,
+                    help='the compressed dataset for evaluating performance in O2C and C2C')
+parser.add_argument('--compressed_dataset_2_type', choices=['files', 'tfrecords'], default='tfrecords')
 parser.add_argument('--downstream_C_model', type=str, required=True)
+parser.add_argument('--skip_C2C', action='store_true')
 parser.add_argument('--output_dir', type=str, required=True)
 
 args = parser.parse_args()
@@ -28,59 +31,77 @@ args = parser.parse_args()
 Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 
 sess = tf.keras.backend.get_session()
-O_data, O_examples = datasets.cifar10.read_images(Path(args.uncompressed_dataset) / 'test')
-O_num_batches = np.ceil(O_examples / args.batch_size).astype(int)
 
-C2O_data = datasets.cifar10.pipeline(O_data,
-                                     batch_size=args.batch_size,
-                                     flip=False,
-                                     crop=False,
-                                     classifier_normalize=True,
-                                     repeat=False)
 
-downstream_O_model = tf.keras.models.load_model(args.downstream_O_model, compile=False)
-if args.downstream_O_model_weights:
-    downstream_O_model.load_weights(args.downstream_O_model_weights)
-downstream_O_model.compile('sgd', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+def load_compressed_dataset(dataset: str, dataset_type: str):
+    if dataset_type == 'tfrecords':
+        _, C_data = datasets.cifar10.read_compressed_tfrecords([dataset])
+        num_examples = sess.run(C_data.reduce(np.int32(0), lambda acc, _: acc + 1))
+        bpp = sess.run(C_data.reduce(np.float32(0.), lambda acc, item: acc + item['range_coded_bpp']) / num_examples)
+    elif dataset_type == 'files':
+        C_data, _ = datasets.cifar10.read_images(Path(dataset) / 'test')
+        num_examples = sess.run(C_data.reduce(np.int32(0), lambda acc, _: acc + 1))
+        bpp = sess.run(datasets.cifar10.count_bpg_bpps(Path(dataset) / 'test'))
+    else:
+        assert False
+
+    mse = C_data.map(lambda item: datasets.get_mse(item, args.uncompressed_dataset), AUTO)
+    mse = sess.run(mse.reduce(np.float32(0.), lambda acc, v: acc + v / num_examples))
+
+    return {'data': C_data,
+            'mse': mse,
+            'bpp': bpp}
+
 
 downstream_C_model = tf.keras.models.load_model(args.downstream_C_model, compile=False)
 downstream_C_model.compile('sgd', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-if args.compressed_dataset_type == 'tfrecords':
-    _, C_data = datasets.cifar10.read_compressed_tfrecords([args.compressed_dataset])
-    bpp = sess.run(C_data.reduce(np.float32(0.), lambda acc, item: acc + item['range_coded_bpp']) / O_examples)
-elif args.compressed_dataset_type == 'files':
-    C_data, _ = datasets.cifar10.read_images(Path(args.compressed_dataset) / 'test')
-    bpp = sess.run(datasets.cifar10.count_bpg_bpps(Path(args.compressed_dataset) / 'test'))
-mse = C_data.map(lambda item: datasets.get_mse(item, args.uncompressed_dataset), AUTO)
-mse = sess.run(mse.reduce(np.float32(0.), lambda acc, v: acc + v / O_examples))
+compressed_dataset_1 = load_compressed_dataset(args.compressed_dataset_1, args.compressed_dataset_1_type)
+compressed_dataset_2 = load_compressed_dataset(args.compressed_dataset_2, args.compressed_dataset_2_type)
 
-O2C_data = datasets.cifar10.pipeline(C_data,
-                                     batch_size=args.batch_size,
-                                     flip=False,
-                                     crop=False,
-                                     classifier_normalize=True,
-                                     correct_bgr=args.downstream_O_model_correct_bgr,
-                                     repeat=False)
-C2C_data = datasets.cifar10.pipeline(C_data,
-                                     batch_size=args.batch_size,
-                                     flip=False,
-                                     crop=False,
-                                     classifier_normalize=True,
-                                     repeat=False)
+if args.downstream_O_model:
+    O_data, _ = datasets.cifar10.read_images(Path(args.uncompressed_dataset) / 'test')
+    C2O_data = datasets.cifar10.pipeline(O_data,
+                                         batch_size=args.batch_size,
+                                         flip=False,
+                                         crop=False,
+                                         classifier_normalize=True,
+                                         repeat=False)
 
-C2C_accuracy = downstream_C_model.evaluate(C2C_data)[1]
-C2O_accuracy = downstream_C_model.evaluate(C2O_data)[1]
-O2C_accuracy = downstream_O_model.evaluate(O2C_data)[1]
+    downstream_O_model = tf.keras.models.load_model(args.downstream_O_model, compile=False)
+    if args.downstream_O_model_weights:
+        downstream_O_model.load_weights(args.downstream_O_model_weights)
+    downstream_O_model.compile('sgd', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-results = [{'dataset': args.compressed_dataset,
-            'bpp': bpp,
-            'mse': mse,
-            'c2o_accuracy': C2O_accuracy,
-            'o2c_accuracy': O2C_accuracy,
-            'c2c_accuracy': C2C_accuracy}]
+results = {'dataset_1': args.compressed_dataset_1,
+           'bpp_1': compressed_dataset_1['bpp'],
+           'mse_1': compressed_dataset_1['mse'],
+           'dataset_2': args.compressed_dataset_2,
+           'bpp_2': compressed_dataset_2['bpp'],
+           'mse_2': compressed_dataset_2['mse'],
+           }
 
-results_df = pd.DataFrame(results)
+if args.downstream_O_model:
+    O2C_data = datasets.cifar10.pipeline(compressed_dataset_2['data'],
+                                         batch_size=args.batch_size,
+                                         flip=False,
+                                         crop=False,
+                                         classifier_normalize=True,
+                                         correct_bgr=args.downstream_O_model_correct_bgr,
+                                         repeat=False)
+    results['c2o_accuracy'] = downstream_C_model.evaluate(C2O_data)[1]
+    results['o2c_accuracy'] = downstream_O_model.evaluate(O2C_data)[1]
+
+if not args.skip_C2C:
+    C2C_data = datasets.cifar10.pipeline(compressed_dataset_2['data'],
+                                         batch_size=args.batch_size,
+                                         flip=False,
+                                         crop=False,
+                                         classifier_normalize=True,
+                                         repeat=False)
+    results['c2c_accuracy'] = downstream_C_model.evaluate(C2C_data)[1]
+
+results_df = pd.DataFrame([results])
 Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 results_df.to_csv(Path(args.output_dir) / 'results.csv', index=False)
 save_experiment_params(Path(args.output_dir), args)
---
+++ scripts/evaluate_all.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+
+EXPERIMENT_DIR=$HOME/thesis-compression-s3/experiments
+BPG_DATASETS=$HOME/thesis-compression-data/datasets/cifar-10-bpg
+EVALUATION_DIR=$HOME/thesis-compression-s3/evaluation
+
+export TF_CPP_MIN_LOG_LEVEL=2
+
+# cifar-vgg16, BPG compression
+for bpg_c_model in $EXPERIMENT_DIR/cifar10_vgg16/bpg/*/; do
+  output_dir="$EVALUATION_DIR/cifar10_vgg16/bpg/$(basename $bpg_c_model)"
+  echo "Processing $output_dir."
+
+  if [ -f $output_dir/results.csv ]; then
+    echo "Skipping: results.csv exists."
+    continue
+  fi
+
+  python evaluator.py --batch_size 2048 \
+    --uncompressed_dataset data/datasets/cifar-10 \
+    --downstream_O_model data/trained_models/cifar-10/vgg16/uncompressed/final_model.hdf5 \
+    --compressed_dataset "$BPG_DATASETS/$(basename $bpg_c_model)" \
+    --compressed_dataset_type files \
+    --downstream_C_model $bpg_c_model/final_model.hdf5 \
+    --output_dir $output_dir
+done
+
+# cifar-vgg16, learned compressor
+for learned_c_model in $EXPERIMENT_DIR/cifar10_vgg16/*/*/; do
+  if [ "$(basename "$(dirname "$learned_c_model")")" == "bpg" ]; then
+    continue
+  fi
+
+  output_dir="$EVALUATION_DIR/cifar10_vgg16/$(basename "$(dirname "$learned_c_model")")/$(basename "$learned_c_model")"
+  echo "Processing $output_dir."
+
+  if [ -f "$output_dir/results.csv" ]; then
+    echo "Skipping: results.csv exists."
+    continue
+  fi
+
+  python evaluator.py --batch_size 2048 \
+    --uncompressed_dataset data/datasets/cifar-10 \
+    --downstream_O_model data/trained_models/cifar-10/vgg16/uncompressed/final_model.hdf5 \
+    --compressed_dataset "$learned_c_model/compressed_cifar10/compressed.tfrecord" \
+    --compressed_dataset_type tfrecords \
+    --downstream_C_model "$learned_c_model/C_model/final_model.hdf5" \
+    --output_dir "$output_dir"
+done
+
+# resnet18, BPG
+for learned_c_model in $EXPERIMENT_DIR/cifar10_resnet18/bpg/*/; do
+  output_dir="$EVALUATION_DIR/cifar10_resnet18/bpg/$(basename $(dirname $learned_c_model))/$(basename $learned_c_model)"
+  echo "Processing $output_dir."
+
+  if [ -f $output_dir/results.csv ]; then
+    echo "Skipping: results.csv exists."
+    continue
+  fi
+
+  python evaluator.py --batch_size 2048 \
+    --uncompressed_dataset data/datasets/cifar-10 \
+    --downstream_O_model data/trained_models/cifar-10/resnet18/uncompressed_bgr/model.hdf5 \
+    --downstream_O_model_weights data/trained_models/cifar-10/resnet18/uncompressed_bgr/final_weights.hdf5 \
+    --downstream_O_model_correct_bgr \
+    --compressed_dataset "$BPG_DATASETS/$(basename $learned_c_model)" \
+    --compressed_dataset_type files \
+    --downstream_C_model $learned_c_model/final_model.hdf5 \
+    --output_dir $output_dir
+done
+
+# resnet18, learned compression
+for learned_c_model in $EXPERIMENT_DIR/cifar10_resnet18/*/*/; do
+  if [ "$(basename "$(dirname "$learned_c_model")")" == "bpg" ]; then
+    continue
+  fi
+
+  output_dir="$EVALUATION_DIR/cifar10_resnet18/$(basename $(dirname $learned_c_model))/$(basename $learned_c_model)"
+  echo "Processing $output_dir."
+
+  if [[ $(basename $learned_c_model) =~ "epoch_check" ]]; then
+    echo "Skipping: this is a probe."
+    continue
+  fi
+
+  if [ -f $output_dir/results.csv ]; then
+    echo "Skipping: results.csv exists."
+    continue
+  fi
+
+  python evaluator.py --batch_size 2048 \
+    --uncompressed_dataset data/datasets/cifar-10 \
+    --downstream_O_model data/trained_models/cifar-10/resnet18/uncompressed_bgr/model.hdf5 \
+    --downstream_O_model_weights data/trained_models/cifar-10/resnet18/uncompressed_bgr/final_weights.hdf5 \
+    --downstream_O_model_correct_bgr \
+    --compressed_dataset $learned_c_model/compressed_cifar10/compressed.tfrecord \
+    --compressed_dataset_type tfrecords \
+    --downstream_C_model $learned_c_model/C_model/final_model.hdf5 \
+    --output_dir $output_dir
+done
+
+# cross-architecture O2C C2O vgg16 to resnet18
+for vgg_c in $EXPERIMENT_DIR/cifar10_vgg16/*/*/; do
+  if [ "$(basename "$(dirname "$vgg_c")")" == "bpg" ]; then
+    continue
+  fi
+
+  vgg_loss=$(basename "$(dirname "$vgg_c")")
+  vgg_loss_param=$(basename "$vgg_c")
+  output_dir="$EVALUATION_DIR/cifar10_Ovgg16_Cresnet18/$vgg_loss/$vgg_loss_param"
+  echo "Processing $output_dir."
+
+  if [ -f "$output_dir/results.csv" ]; then
+    echo "Skipping: results.csv exists."
+    continue
+  fi
+
+  python evaluator.py --batch_size 2048 \
+    --uncompressed_dataset data/datasets/cifar-10 \
+    --downstream_O_model data/trained_models/cifar-10/resnet18/uncompressed_bgr/model.hdf5 \
+    --downstream_O_model_weights data/trained_models/cifar-10/resnet18/uncompressed_bgr/final_weights.hdf5 \
+    --downstream_O_model_correct_bgr \
+    --compressed_dataset_1 "$vgg_c/compressed_cifar10/compressed.tfrecord" \
+    --compressed_dataset_1_type tfrecords \
+    --compressed_dataset_2 "$vgg_c/compressed_cifar10/compressed.tfrecord" \
+    --compressed_dataset_2_type tfrecords \
+    --skip_C2C \
+    --downstream_C_model "$vgg_c/C_model/final_model.hdf5" \
+    --output_dir "$output_dir"
+done
+
+# cross-architecture O2C C2O resnet18 to vgg16
+for resnet_c in $EXPERIMENT_DIR/cifar10_resnet18/*/*/; do
+  if [ "$(basename "$(dirname "$resnet_c")")" == "bpg" ]; then
+    continue
+  fi
+
+  resnet_loss=$(basename "$(dirname "$resnet_c")")
+  resnet_loss_param=$(basename "$resnet_c")
+  output_dir="$EVALUATION_DIR/cifar10_Oresnet18_Cvgg16/$resnet_loss/$resnet_loss_param"
+  echo "Processing $output_dir."
+
+  if [[ "$resnet_loss_param" =~ "epoch_check" ]]; then
+    echo "Skipping: this is a probe."
+    continue
+  fi
+
+  if [ -f "$output_dir/results.csv" ]; then
+    echo "Skipping: results.csv exists."
+    continue
+  fi
+
+  python evaluator.py --batch_size 2048 \
+    --uncompressed_dataset data/datasets/cifar-10 \
+    --downstream_O_model data/trained_models/cifar-10/vgg16/uncompressed/final_model.hdf5 \
+    --compressed_dataset_1 "$resnet_c/compressed_cifar10/compressed.tfrecord" \
+    --compressed_dataset_1_type tfrecords \
+    --compressed_dataset_2 "$resnet_c/compressed_cifar10/compressed.tfrecord" \
+    --compressed_dataset_2_type tfrecords \
+    --skip_C2C \
+    --downstream_C_model "$resnet_c/C_model/final_model.hdf5" \
+    --output_dir "$output_dir"
+done
+
+# cross-architecture C2C
+for resnet_c in $EXPERIMENT_DIR/cifar10_resnet18/*/*/; do
+  if [ "$(basename "$(dirname "$resnet_c")")" == "bpg" ]; then
+    continue
+  fi
+
+  if [[ "$(basename "$resnet_c")" =~ "epoch_check" ]]; then
+    continue
+  fi
+
+  resnet_loss=$(basename "$(dirname "$resnet_c")")
+  resnet_loss_param="$(basename "$resnet_c")"
+
+  for vgg_c in $EXPERIMENT_DIR/cifar10_vgg16/*/*/; do
+    if [ "$(basename "$(dirname "$vgg_c")")" == "bpg" ]; then
+      continue
+    fi
+
+    vgg_loss=$(basename "$(dirname "$vgg_c")")
+    vgg_loss_param=$(basename "$vgg_c")
+
+
+    output_dir="$EVALUATION_DIR/cifar10_Cresnet18_Cvgg16/$resnet_loss_param/$vgg_loss_param"
+    if [ -f "$output_dir/results.csv" ]; then
+      echo "Skipping: results.csv exists."
+      continue
+    fi
+
+    python evaluator.py --batch_size 2048 \
+      --uncompressed_dataset data/datasets/cifar-10 \
+      --compressed_dataset_1 "$resnet_c/compressed_cifar10/compressed.tfrecord" \
+      --compressed_dataset_1_type tfrecords \
+      --compressed_dataset_2 "$vgg_c/compressed_cifar10/compressed.tfrecord" \
+      --compressed_dataset_2_type tfrecords \
+      --downstream_C_model "$resnet_c/C_model/final_model.hdf5" \
+      --output_dir "$output_dir"
+
+    output_dir="$EVALUATION_DIR/cifar10_Cvgg16_Cresnet18/$vgg_loss_param/$resnet_loss_param"
+    if [ -f "$output_dir/results.csv" ]; then
+      echo "Skipping: results.csv exists."
+      continue
+    fi
+
+    python evaluator.py --batch_size 2048 \
+      --uncompressed_dataset data/datasets/cifar-10 \
+      --compressed_dataset_1 "$vgg_c/compressed_cifar10/compressed.tfrecord" \
+      --compressed_dataset_1_type tfrecords \
+      --compressed_dataset_2 "$resnet_c/compressed_cifar10/compressed.tfrecord" \
+      --compressed_dataset_2_type tfrecords \
+      --downstream_C_model "$vgg_c/C_model/final_model.hdf5" \
+      --output_dir "$output_dir"
+  done
+done
